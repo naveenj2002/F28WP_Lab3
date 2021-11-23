f3c6a5e7830d9881cd9a0e7598022450a92cf21b
naveenj2002 <91015730+naveenj2002@users.noreply.github.com>
2021-11-23 10:41:11 +0400

    Completed Lab 3

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+//creating app
+const app = express();
+
+// using JSON and URL Encoded middleware
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+//send the index.html when receiving HTTP GET /
+app.use(express.static('public')); 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname }); });
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+console.log(`Cart app listening at http://localhost:${port}`);
+});
+//handling static HTML and EJS templates
+app.use(express.static('public')); 
+app.set('view engine', 'ejs'); 
+app.get('/', (req, res) => {
+    res.render('index'); //no need for ejs extension 
+});
+//route for contacts
+app.get('/contacts', (req, res) => { 
+    res.render('contacts');
+});
+//Render the contacts file in the /api/register route
+app.get("/register", (req, res) => {
+    res.render('register');
+});
+//Render the contacts file in the /api/login route
+app.get("/login", (req, res) => {
+    res.render('login');
+});
+//pass requests to the router middleware
+const router = require('./routes/apis'); 
+app.use(router);
+
+const session = require('express-session');
+app.use(session({secret: 'some secret code'}));
---
+++ db/dbQuery.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql');
+const databasename = "sql6440943";
+
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: "sql6.freemysqlhosting.net",
+    user: "sql6451470",
+    password: "nZpFdF6yns",
+    database: "sql6451470",
+    debug: true
+});
+
+function executeQuery(query, callback) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            return callback(err, null);
+        } else if (connection) {
+            connection.query(query, function(err, rows, fields) {
+                connection.release();
+                if (err) {
+                    return callback(err, null);
+                }
+                return callback(null, rows);
+            });
+        } else {
+            return callback(true, "No Connection");
+        }
+    });
+}
+
+function getResult(query, callback) {
+    executeQuery(query, function(err, rows) {
+        if (!err) {
+            callback(null, rows);
+        } else {
+            callback(true, err);
+        }
+    });
+}
+
+module.exports = {
+    getResult
+};
